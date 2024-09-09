b857999910bf7908e0c7b2a0eb57a6358592fa2c
Johnny Bhojwani <johnny_bhojwani@baylor.edu>
2024-09-09 15:01:01 -0500

    added write_philosophers.py file

---
+++ write_philosophers.py
@@ -0,0 +1,15 @@
+def main():
+    #open a file named philosophers.txt
+    outfile = 
+
+
+    #write the names of three philosophers to the file
+    #John Locke, David Hume and Edmund Burke
+
+
+
+    #close the file
+
+
+#call the main function
+main()
