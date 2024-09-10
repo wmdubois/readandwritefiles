e9204e2a8dcafc6c5c01f693a5a7b25ff8d22325
Will DuBois <will_dubois1@baylor.edu>
2024-09-10 14:34:29 -0500

    Completed write_philosophers exercise

--- write_philosophers.py
+++ write_philosophers.py
@@ -1,15 +1,18 @@
 def main():
     #open a file named philosophers.txt
-    outfile = 
+    outfile = open('philosophers.txt','w')
 
 
     #write the names of three philosophers to the file
     #John Locke, David Hume and Edmund Burke
 
-
+    outfile.write('John Locke' + '\n')
+    outfile.write('David Hume' + '\n')
+    outfile.write('Edmund Burke' + '\n')
 
     #close the file
 
+    outfile.close()
 
 #call the main function
 main()
