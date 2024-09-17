f9354bab100d8fdd43bd51750d7760103df7adff
Will DuBois <will_dubois1@baylor.edu>
2024-09-17 16:28:03 -0500

    employee_bonus.py written

--- customer_country.py
+++ customer_country.py
@@ -1,10 +1,10 @@
 import csv
 
 def main():
-    file_obj = open('customers.csv','r')
+    infile = open('customers.csv','r')
     outfile = open('customer_country.csv','w')
 
-    for line in file_obj:
+    for line in infile:
         line = line.split(",")
         outfile.write(f"{line[1]} {line[2]}, {line[4]}" + "\n")
 
---
+++ employee_bonus.py
@@ -0,0 +1,32 @@
+print()
+# ID,Name,Age,Salary,HoursWorked,Productivity,Team,Bonus
+#name - 1
+#salary - 3
+#bonus - 7
+#pay - salary + bonus
+
+import csv
+
+def main():
+    infile = open("employee_data.csv","r")
+    category = infile.readline().rstrip("\n").split(",")
+
+    for line in infile:
+        line = line.rstrip("\n").split(",")
+        bonus_value = (float(line[7]) * float(line[3]))
+        pay_value = (float(line[3]) + bonus_value)
+
+
+        print(f"{category[1]}: {line[1]}")
+        print(f"{category[3]}: {'${:,.2f}'.format(float(line[3]))}")
+        print(f"{category[7]}: {'${:,.2f}'.format(bonus_value)}")
+        print(f"Pay: {'${:,.2f}'.format(pay_value)}")
+        print()
+
+    
+
+
+
+
+
+main()
