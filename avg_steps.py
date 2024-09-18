6db0e1c6bafb950196fe28dca49fb56fcc9a92b9
Will DuBois <will_dubois1@baylor.edu>
2024-09-17 19:18:29 -0500

    avg_steps.py written

--- avg_steps.py
+++ avg_steps.py
@@ -5,22 +5,26 @@ import csv
 def main():
     infile = open("steps.csv","r")
     infile.readline()
-    month = ['January','February','March','April','May','June','July','August','September','October','November','December']
+    month_name = ['January','February','March','April','May','June','July','August','September','October','November','December']
+    month_steps = [0,0,0,0,0,0,0,0,0,0,0,0]
+    month_days = [0,0,0,0,0,0,0,0,0,0,0,0]
+    avg_steps = [0,0,0,0,0,0,0,0,0,0,0,0]
 
-
+    x=0
     for line in infile:
         line = line.rstrip("\n").split(",")
-        #print(line)
-        while line[1] == '1':
-            
+    
+        if x + 1 == int(line[0]):
+            month_steps[x] += int(line[1])
+            month_days[x] += 1
+        else:
+            x+=1
+            month_steps[x] += int(line[1])
+            month_days[x] += 1
 
-
-
-
-
-
-
-
-
+    for s in range(12):
+        avg_steps[s] += (month_steps[s] / month_days[s])
+        print(f"{month_name[s]} - {'{:,.2f}'.format(avg_steps[s])}")
+                   
 
 main()
